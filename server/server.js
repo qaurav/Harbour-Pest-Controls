1aef8fc5d0a6b2d183c19d74356e5bc823696ad8
gaurav neupane <gauravneupane2021@gmail.com>
2025-03-28 08:50:26 +0545

    minor syntax change

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 });
 
 // Serve static files
-app.use(express.static(--dirname,'public', {
+app.use(express.static(__dirname,'public', {
   maxAge: '1y',
   setHeaders: (res, path) => {
     if (path.endsWith('.html')) {
